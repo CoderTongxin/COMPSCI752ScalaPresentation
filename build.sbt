4d1d70011ce29d74e68cf7fe3632f9ac9efe22d3
CoderTongxin <gin199413@163.com>
2018-05-19 22:04:03 +1200

    Initiate project

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "752presentation"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,6 @@
+
+object Main extends App {
+  val point1 = new Point(2, 3)
+  point1.x
+  println(point1)
+}
